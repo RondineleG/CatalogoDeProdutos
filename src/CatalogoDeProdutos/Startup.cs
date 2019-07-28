5b6521e7692ee4128b20e330ed522ba96fd12425
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-07-28 00:30:50 -0300

    Configurando Startup para usar o MVC

--- src/CatalogoDeProdutos/Startup.cs
+++ src/CatalogoDeProdutos/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using CatalogoDeProdutos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,15 +15,15 @@ namespace CatalogoDeProdutos
         }
 
         public IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CatalogoDataContext>(options => options.UseSqlServer
             (Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -32,10 +31,7 @@ namespace CatalogoDeProdutos
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
