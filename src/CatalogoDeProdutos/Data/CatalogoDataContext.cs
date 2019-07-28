45fcffff18835cceac9168023fdd09f514013988
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-07-27 22:30:31 -0300

    Criando DataContext

---
+++ src/CatalogoDeProdutos/Data/CatalogoDataContext.cs
@@ -0,0 +1,25 @@
+using CatalogoDeProdutos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoDeProdutos.Data
+{
+    public class CatalogoDataContext : DbContext
+    {
+        public DbSet<Categoria> Categorias {get;set;}
+
+        public DbSet<Produto> Produtos {get;set;}
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=prodcat;User ID=SA;Password=1q2w3e%&!");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           builder.ApplyConfiguration(new ProdutoMap());
+           builder.ApplyConfiguration(new CategoriaMap());
+
+        }
+    }
+}
